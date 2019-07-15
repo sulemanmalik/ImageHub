f92c796f9a3c98f74ab7c0ba955d9603f16cf4c2
sulemanmalik <sulemanmalik@Sulemans-MBP.lan>
2019-07-15 10:59:10 -0400

    chore: cleanup

--- src/models/Image.js
+++ src/models/Image.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const imageSchema = new Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   title: {
     type: String,
     required: true
